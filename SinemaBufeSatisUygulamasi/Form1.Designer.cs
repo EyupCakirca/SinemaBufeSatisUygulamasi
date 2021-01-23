1c78ea202e87c3f5482c94c57b1702d05bcecdb7
Eyüp ÇAKIRCA <turkuaz_345@hotmail.com>
2021-01-23 20:43:29 +0300

    Add project files.

---
+++ SinemaBufeSatisUygulamasi/Form1.Designer.cs
@@ -0,0 +1,353 @@
+﻿
+namespace SinemaBufeSatisUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gbxUrunlerinMiktari = new System.Windows.Forms.GroupBox();
+            this.tbxBilet = new System.Windows.Forms.TextBox();
+            this.lblBilet = new System.Windows.Forms.Label();
+            this.tbxCay = new System.Windows.Forms.TextBox();
+            this.lblCay = new System.Windows.Forms.Label();
+            this.tbxSu = new System.Windows.Forms.TextBox();
+            this.lblSu = new System.Windows.Forms.Label();
+            this.tbxMisir = new System.Windows.Forms.TextBox();
+            this.lblMisir = new System.Windows.Forms.Label();
+            this.gbxFiyatTablosu = new System.Windows.Forms.GroupBox();
+            this.lblBiletFiyat = new System.Windows.Forms.Label();
+            this.lblCayFiyat = new System.Windows.Forms.Label();
+            this.lblSuFiyat = new System.Windows.Forms.Label();
+            this.lblMisirFiyat = new System.Windows.Forms.Label();
+            this.gbxKasa = new System.Windows.Forms.GroupBox();
+            this.lblKToplam = new System.Windows.Forms.Label();
+            this.lblKasaToplam = new System.Windows.Forms.Label();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.lblTop = new System.Windows.Forms.Label();
+            this.lblToplam = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.gbxUrunlerinMiktari.SuspendLayout();
+            this.gbxFiyatTablosu.SuspendLayout();
+            this.gbxKasa.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbxUrunlerinMiktari
+            // 
+            this.gbxUrunlerinMiktari.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.gbxUrunlerinMiktari.Controls.Add(this.tbxBilet);
+            this.gbxUrunlerinMiktari.Controls.Add(this.lblBilet);
+            this.gbxUrunlerinMiktari.Controls.Add(this.tbxCay);
+            this.gbxUrunlerinMiktari.Controls.Add(this.lblCay);
+            this.gbxUrunlerinMiktari.Controls.Add(this.tbxSu);
+            this.gbxUrunlerinMiktari.Controls.Add(this.lblSu);
+            this.gbxUrunlerinMiktari.Controls.Add(this.tbxMisir);
+            this.gbxUrunlerinMiktari.Controls.Add(this.lblMisir);
+            this.gbxUrunlerinMiktari.Location = new System.Drawing.Point(31, 247);
+            this.gbxUrunlerinMiktari.Name = "gbxUrunlerinMiktari";
+            this.gbxUrunlerinMiktari.Size = new System.Drawing.Size(446, 343);
+            this.gbxUrunlerinMiktari.TabIndex = 0;
+            this.gbxUrunlerinMiktari.TabStop = false;
+            this.gbxUrunlerinMiktari.Text = "Ürünlerin Miktarı";
+            // 
+            // tbxBilet
+            // 
+            this.tbxBilet.Location = new System.Drawing.Point(159, 269);
+            this.tbxBilet.Name = "tbxBilet";
+            this.tbxBilet.Size = new System.Drawing.Size(201, 30);
+            this.tbxBilet.TabIndex = 7;
+            // 
+            // lblBilet
+            // 
+            this.lblBilet.AutoSize = true;
+            this.lblBilet.Location = new System.Drawing.Point(40, 275);
+            this.lblBilet.Name = "lblBilet";
+            this.lblBilet.Size = new System.Drawing.Size(41, 24);
+            this.lblBilet.TabIndex = 6;
+            this.lblBilet.Text = "Bilet";
+            // 
+            // tbxCay
+            // 
+            this.tbxCay.Location = new System.Drawing.Point(159, 203);
+            this.tbxCay.Name = "tbxCay";
+            this.tbxCay.Size = new System.Drawing.Size(201, 30);
+            this.tbxCay.TabIndex = 5;
+            // 
+            // lblCay
+            // 
+            this.lblCay.AutoSize = true;
+            this.lblCay.Location = new System.Drawing.Point(40, 209);
+            this.lblCay.Name = "lblCay";
+            this.lblCay.Size = new System.Drawing.Size(40, 24);
+            this.lblCay.TabIndex = 4;
+            this.lblCay.Text = "Çay";
+            // 
+            // tbxSu
+            // 
+            this.tbxSu.Location = new System.Drawing.Point(159, 135);
+            this.tbxSu.Name = "tbxSu";
+            this.tbxSu.Size = new System.Drawing.Size(201, 30);
+            this.tbxSu.TabIndex = 3;
+            // 
+            // lblSu
+            // 
+            this.lblSu.AutoSize = true;
+            this.lblSu.Location = new System.Drawing.Point(40, 141);
+            this.lblSu.Name = "lblSu";
+            this.lblSu.Size = new System.Drawing.Size(30, 24);
+            this.lblSu.TabIndex = 2;
+            this.lblSu.Text = "Su";
+            // 
+            // tbxMisir
+            // 
+            this.tbxMisir.Location = new System.Drawing.Point(159, 71);
+            this.tbxMisir.Name = "tbxMisir";
+            this.tbxMisir.Size = new System.Drawing.Size(201, 30);
+            this.tbxMisir.TabIndex = 1;
+            // 
+            // lblMisir
+            // 
+            this.lblMisir.AutoSize = true;
+            this.lblMisir.Location = new System.Drawing.Point(40, 77);
+            this.lblMisir.Name = "lblMisir";
+            this.lblMisir.Size = new System.Drawing.Size(48, 24);
+            this.lblMisir.TabIndex = 0;
+            this.lblMisir.Text = "Mısır";
+            // 
+            // gbxFiyatTablosu
+            // 
+            this.gbxFiyatTablosu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.gbxFiyatTablosu.Controls.Add(this.lblBiletFiyat);
+            this.gbxFiyatTablosu.Controls.Add(this.lblCayFiyat);
+            this.gbxFiyatTablosu.Controls.Add(this.lblSuFiyat);
+            this.gbxFiyatTablosu.Controls.Add(this.lblMisirFiyat);
+            this.gbxFiyatTablosu.Location = new System.Drawing.Point(522, 247);
+            this.gbxFiyatTablosu.Name = "gbxFiyatTablosu";
+            this.gbxFiyatTablosu.Size = new System.Drawing.Size(168, 342);
+            this.gbxFiyatTablosu.TabIndex = 1;
+            this.gbxFiyatTablosu.TabStop = false;
+            this.gbxFiyatTablosu.Text = "Fiyat Tablosu";
+            // 
+            // lblBiletFiyat
+            // 
+            this.lblBiletFiyat.AutoSize = true;
+            this.lblBiletFiyat.Location = new System.Drawing.Point(43, 275);
+            this.lblBiletFiyat.Name = "lblBiletFiyat";
+            this.lblBiletFiyat.Size = new System.Drawing.Size(77, 24);
+            this.lblBiletFiyat.TabIndex = 3;
+            this.lblBiletFiyat.Text = "Bilet:20Tl";
+            // 
+            // lblCayFiyat
+            // 
+            this.lblCayFiyat.AutoSize = true;
+            this.lblCayFiyat.Location = new System.Drawing.Point(48, 209);
+            this.lblCayFiyat.Name = "lblCayFiyat";
+            this.lblCayFiyat.Size = new System.Drawing.Size(67, 24);
+            this.lblCayFiyat.TabIndex = 2;
+            this.lblCayFiyat.Text = "Çay:5Tl";
+            // 
+            // lblSuFiyat
+            // 
+            this.lblSuFiyat.AutoSize = true;
+            this.lblSuFiyat.Location = new System.Drawing.Point(53, 141);
+            this.lblSuFiyat.Name = "lblSuFiyat";
+            this.lblSuFiyat.Size = new System.Drawing.Size(57, 24);
+            this.lblSuFiyat.TabIndex = 1;
+            this.lblSuFiyat.Text = "Su:2Tl";
+            // 
+            // lblMisirFiyat
+            // 
+            this.lblMisirFiyat.AutoSize = true;
+            this.lblMisirFiyat.Location = new System.Drawing.Point(39, 77);
+            this.lblMisirFiyat.Name = "lblMisirFiyat";
+            this.lblMisirFiyat.Size = new System.Drawing.Size(84, 24);
+            this.lblMisirFiyat.TabIndex = 0;
+            this.lblMisirFiyat.Text = "Mısır:15Tl";
+            // 
+            // gbxKasa
+            // 
+            this.gbxKasa.BackColor = System.Drawing.Color.Red;
+            this.gbxKasa.Controls.Add(this.btnTemizle);
+            this.gbxKasa.Controls.Add(this.lblKToplam);
+            this.gbxKasa.Controls.Add(this.lblKasaToplam);
+            this.gbxKasa.Controls.Add(this.btnHesapla);
+            this.gbxKasa.Controls.Add(this.lblTop);
+            this.gbxKasa.Controls.Add(this.lblToplam);
+            this.gbxKasa.Location = new System.Drawing.Point(731, 247);
+            this.gbxKasa.Name = "gbxKasa";
+            this.gbxKasa.Size = new System.Drawing.Size(236, 341);
+            this.gbxKasa.TabIndex = 2;
+            this.gbxKasa.TabStop = false;
+            this.gbxKasa.Text = "Kasa";
+            // 
+            // lblKToplam
+            // 
+            this.lblKToplam.AutoSize = true;
+            this.lblKToplam.Location = new System.Drawing.Point(139, 269);
+            this.lblKToplam.Name = "lblKToplam";
+            this.lblKToplam.Size = new System.Drawing.Size(41, 24);
+            this.lblKToplam.TabIndex = 4;
+            this.lblKToplam.Text = "00Tl";
+            // 
+            // lblKasaToplam
+            // 
+            this.lblKasaToplam.AutoSize = true;
+            this.lblKasaToplam.Location = new System.Drawing.Point(34, 269);
+            this.lblKasaToplam.Name = "lblKasaToplam";
+            this.lblKasaToplam.Size = new System.Drawing.Size(107, 24);
+            this.lblKasaToplam.TabIndex = 3;
+            this.lblKasaToplam.Text = "Kasa Toplam";
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(38, 153);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(141, 40);
+            this.btnHesapla.TabIndex = 2;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // lblTop
+            // 
+            this.lblTop.AutoSize = true;
+            this.lblTop.Location = new System.Drawing.Point(139, 111);
+            this.lblTop.Name = "lblTop";
+            this.lblTop.Size = new System.Drawing.Size(41, 24);
+            this.lblTop.TabIndex = 1;
+            this.lblTop.Text = "00Tl";
+            // 
+            // lblToplam
+            // 
+            this.lblToplam.AutoSize = true;
+            this.lblToplam.Location = new System.Drawing.Point(34, 111);
+            this.lblToplam.Name = "lblToplam";
+            this.lblToplam.Size = new System.Drawing.Size(65, 24);
+            this.lblToplam.TabIndex = 0;
+            this.lblToplam.Text = "Toplam";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(51, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(594, 46);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "SİNEMA & BÜFE SATIŞ PANELİ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(691, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(275, 183);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.Location = new System.Drawing.Point(38, 201);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(141, 40);
+            this.btnTemizle.TabIndex = 5;
+            this.btnTemizle.Text = "Temizle";
+            this.btnTemizle.UseVisualStyleBackColor = true;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.panel1.Location = new System.Drawing.Point(-3, 633);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1007, 44);
+            this.panel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Magenta;
+            this.ClientSize = new System.Drawing.Size(1000, 675);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gbxKasa);
+            this.Controls.Add(this.gbxFiyatTablosu);
+            this.Controls.Add(this.gbxUrunlerinMiktari);
+            this.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Sinema Büfe Satış Uygulaması";
+            this.gbxUrunlerinMiktari.ResumeLayout(false);
+            this.gbxUrunlerinMiktari.PerformLayout();
+            this.gbxFiyatTablosu.ResumeLayout(false);
+            this.gbxFiyatTablosu.PerformLayout();
+            this.gbxKasa.ResumeLayout(false);
+            this.gbxKasa.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxUrunlerinMiktari;
+        private System.Windows.Forms.TextBox tbxBilet;
+        private System.Windows.Forms.Label lblBilet;
+        private System.Windows.Forms.TextBox tbxCay;
+        private System.Windows.Forms.Label lblCay;
+        private System.Windows.Forms.TextBox tbxSu;
+        private System.Windows.Forms.Label lblSu;
+        private System.Windows.Forms.TextBox tbxMisir;
+        private System.Windows.Forms.Label lblMisir;
+        private System.Windows.Forms.GroupBox gbxFiyatTablosu;
+        private System.Windows.Forms.Label lblBiletFiyat;
+        private System.Windows.Forms.Label lblCayFiyat;
+        private System.Windows.Forms.Label lblSuFiyat;
+        private System.Windows.Forms.Label lblMisirFiyat;
+        private System.Windows.Forms.GroupBox gbxKasa;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label lblTop;
+        private System.Windows.Forms.Label lblToplam;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblKToplam;
+        private System.Windows.Forms.Label lblKasaToplam;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ SinemaBufeSatisUygulamasi/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SinemaBufeSatisUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int _kasaTutar = 0;
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            int misir, su, bilet, cay, toplam;
+            misir = Convert.ToInt32(tbxMisir.Text);
+            su = Convert.ToInt32(tbxSu.Text);
+            cay = Convert.ToInt32(tbxCay.Text);
+            bilet = Convert.ToInt32(tbxBilet.Text);
+
+            toplam = misir * 15 + su * 2 + cay * 5 + bilet * 20;
+
+            lblTop.Text = toplam.ToString() + "Tl";
+
+            _kasaTutar =_kasaTutar + toplam;
+            lblKToplam.Text = _kasaTutar.ToString() + "Tl";
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            tbxBilet.Text = " ";
+            tbxCay.Text = " ";
+            tbxMisir.Text = " ";
+            tbxSu.Text = " ";
+            tbxMisir.Focus();
+        }
+    }
+}
